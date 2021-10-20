f1ff89b73970ee02c3b664333a3e687aed224eef
Andrey_Fesiuk <fesiukandrey@gmail.com>
2021-10-20 23:13:32 +0300

    Lab1 done

---
+++ Lab1/adslab1ex1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+namespace adslab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводжу змінні
+            double x, y, z;
+            double a, b;
+            //блок зчитування даних з консолі
+            Console.Write("Input x :");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Input y:");
+            y = double.Parse(Console.ReadLine());
+            Console.Write("Input z :");
+            z = double.Parse(Console.ReadLine());
+            //Обрахунок результату в залежності від вхідних даних
+            if ((z == 0) || (y * z == 0) || (x == 0) || (x % PI == 0))//Перевірка на ОДЗ
+                WriteLine("Помилка!На нуль ділити не можна!");
+            else
+            {
+                //Обрахунок результату для виразів
+                a = ((1 / (2 * Sin(PI + x))) + Pow(Sin((x + y) / z), 2));
+                b = (Cos (Pow(a,2) * x) )/ (2 * y * z);
+                //виведення результату обрахунків на консоль
+                Console.WriteLine("Result of a is:" + a.ToString());
+                Console.WriteLine("Result of b is:" + b.ToString());
+            }
+        }
+    }
+}
---
+++ Lab1/adslab1ex2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+namespace ex2ads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, m;
+            Write("n = ");
+            n = Convert.ToInt32(ReadLine());
+            Write("m = ");
+            m = Convert.ToInt32(ReadLine());
+            int summ1,summ2;
+            for (int z = n; z <= m; z++)
+            {
+                summ1 = 0;
+                for (int i = 1; i < z; i++)
+                {
+                    if (z % i == 0)
+                        summ1 += i;
+                }
+
+                summ2 = 0;
+                for (int k = 1; k < summ1; k++)
+                {
+                    if (summ1 % k == 0)
+                        summ2 += k;
+                }
+                if (summ2 == z && summ1 != z && summ1 > z)
+                    WriteLine("Числа {0} i {1} дружнi",z,summ1);;
+
+
+            }
+        }
+    }
+}
