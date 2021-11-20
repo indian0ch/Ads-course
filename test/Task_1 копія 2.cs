d2fac2ef0d48cd40d8aaddb407c941a36d24f67b
Andrey_Fesiuk <fesiukandrey@gmail.com>
2021-11-20 19:42:49 +0200

    Test csproj

---
+++ test/Task_1 копія 2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводжу змінні
+            int n;
+            double a;
+
+            //Блок зчитування даних з консолі
+            Write("Input n :"); n = Convert.ToInt32(ReadLine());
+            double summ = 0;
+            //loop
+            for (double k = 1; k <= n; k++)
+            {
+                if (k % 2 == 0)//parne
+                {
+                    a = (1 / ((2 * k*k) + k) );
+                    
+                }
+                else
+                {
+                    a = (-1 / ((2 * k*k) + k) );
+                    
+                }
+                summ += a;
+
+            }
+            //export result in console
+            WriteLine("Summ is{0}", summ);
+        }
+    }
+}
+
