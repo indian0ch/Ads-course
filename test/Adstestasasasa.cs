2270825882e0ab87e02029ef94a4fd1a5dc2cc86
AndriykoF <fesiukandrey@gmail.com>
2021-11-21 01:18:21 +0200

    testik

---
+++ test/Adstestasasasa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводжу змінні
+            int n;
+            double a, a1=0;
+            Random rnd = new Random();
+            //Блок зчитування даних з консолі
+            Write("Input n :"); n = Convert.ToInt32(ReadLine());
+            
+            //
+            WriteLine("Послідовність матиме такий вигляд:");
+            for (int i = 1; i <= n; i++)
+            {
+                a = rnd.Next(0, 100) + rnd.NextDouble();
+
+                a1 += a;
+                Write("{0};", a1) ;
+            }
+        }
+    }
+}
