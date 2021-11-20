61acb15879decbf1298862a9b1a8bda4d0142a8f
AndriykoF <fesiukandrey@gmail.com>
2021-11-21 01:29:14 +0200

    test finished

---
+++ test/testik.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//
+            double a;
+            int n;
+            //
+            Write("Input n :"); n = Convert.ToInt32(ReadLine());
+            Write("Input a = "); a = Convert.ToInt32(ReadLine());
+            double summ = 0;
+            //цикл
+            double a1 = 1;
+            for (int i =0;i<=n;i++)
+            {
+
+                if (i <= 1)
+                {
+                    a1 *= (1 / a);
+                    summ += a1;
+                }
+                else 
+                {
+                    a1 *= (1 / (a * a));
+
+                    summ += a1;
+                }
+            }
+            //Виведення результату на консоль
+            WriteLine("Summ of this : " + summ.ToString());
+            ReadKey();
+        }
+    }
+}
